47e5c92f687243465b7e7c8c977a6ed3f5a85333
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-09-21 16:15:06 -0400

    Add state & context to enable accordion functionality

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+// NOTE: accordion items get wider when you open the body!!!
+
 export const Container = styled.section`
   display: flex;
   border-bottom: 8px solid #222;
@@ -7,6 +9,7 @@ export const Container = styled.section`
 
 export const Frame = styled.div`
   margin-bottom: 40px;
+  max-width: 1200px;
 `
 
 export const Inner = styled.div`
@@ -43,6 +46,7 @@ export const Header = styled.div`
   display: flex;
   justify-content: space-between;
   cursor: pointer;
+  box-sizing: border-box;
   margin-bottom: 1px;
   font-size: 26px;
   font-weight: normal;
@@ -50,14 +54,25 @@ export const Header = styled.div`
   padding: 0.8em 1.2em 0.8em 1.2em;
   user-select: none;
   align-items: center;
+  width: 100%;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+
+    @media (max-width: 600px) {
+      width: 16px;
+    }
+  }
 
   @media (max-width: 600px) {
     font-size: 16px;
   }
 `
 
+// NOTE: transition doesn't do anything - no max-height specified. Will need to pass in prop to make this work
 export const Body = styled.div`
-  max-width: 1200px;
+  box-sizing: border-box;
   transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
   font-size: 26px;
   font-weight: normal;
@@ -66,6 +81,7 @@ export const Body = styled.div`
   padding: 0.8em 1.2em 0.8em 1.2em;
   user-select: none;
   align-items: center;
+  width: 100%;
 
   @media (max-width: 600px) {
     font-size: 16px;
