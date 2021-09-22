8f370a739aebf45f25df05db9e98d07a3df7725f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-09-22 14:25:19 -0400

    Create signin page skeleton

---
+++ src/components/form/styles/form.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import { Link as ReachRouterLink } from 'react-router-dom'
+// create alias to avoid name clash with Link component
+
+export const Container = styled.section``
+
+export const Error = styled.div``
+
+export const Base = styled.form``
+
+export const Title = styled.h1``
+
+export const Text = styled.h1``
+
+export const TextSmall = styled.h2``
+
+export const Link = styled(ReachRouterLink)``
+
+export const Input = styled.input``
+
+export const Submit = styled.button``
