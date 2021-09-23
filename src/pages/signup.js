c5a351ec7b41b997796cfeddf529b1d4ef86643e
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-09-23 13:43:35 -0400

    Add signup & browse pages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,34 +1,38 @@
 import React, { useState } from 'react'
-import { Form } from '../components'
 import { HeaderContainer } from '../containers/header'
+import { Form } from '../components'
+import * as ROUTES from '../constants/routes'
 import { FooterContainer } from '../containers/footer'
 
-export default function Signin() {
-  const [error, setError] = useState('')
+export default function Signup() {
+  const [firstName, setFirstName] = useState('')
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
-  const isInvalid = password === '' || emailAddress === ''
+  const isInvalid = firstName === '' || password === '' || emailAddress === ''
   // disables submit button for empty input; use required attr instead???
   // NOTE: add full email address validation?
 
-  const handleSignin = event => {
+  const handleSignup = event => {
     event.preventDefault()
-
-    // call in here to firebase to authenticate the user
-    // if there's an error, populate the error state
   }
 
   return (
     <>
       <HeaderContainer>
         <Form>
-          <Form.Title>Sign In</Form.Title>
+          <Form.Title>Sign Up</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
 
-          <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Base onSubmit={handleSignup} method="POST">
             <Form.Input
-              placeholder="Email address"
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email Address"
               value={emailAddress}
               onChange={({ target }) => setEmailAddress(target.value)}
             />
@@ -40,11 +44,11 @@ export default function Signin() {
               onChange={({ target }) => setPassword(target.value)}
             />
             <Form.Submit disabled={isInvalid} type="submit">
-              Sign In
+              Sign Up
             </Form.Submit>
 
             <Form.Text>
-              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+              Already a user? <Form.Link to="/signin">Sign up now.</Form.Link>
             </Form.Text>
             <Form.TextSmall>
               This page is protected by Google reCAPTCHA.
