a88c2754e434cafb405a8f714b480eb98bdc5672
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-10-19 15:30:43 -0400

    Expand browse page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { Link as ReachRouterLink } from 'react-router-dom'
 // using alias to avoid name clash with Link component
 
-import { Background, Container, Logo, ButtonLink } from './styles/header'
+import {
+  Group,
+  Background,
+  Link,
+  Text,
+  Container,
+  Logo,
+  ButtonLink,
+} from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children
@@ -12,12 +20,24 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>
 }
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>
+}
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
     <ReachRouterLink to={to}>
       <Logo {...restProps} />
     </ReachRouterLink>
   )
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>
 }
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
